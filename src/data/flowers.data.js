ca165e39e2bae69eed2808fd30012837d7c3634f
Alon Mlievski <alonmlievski@gmail.com>
2023-07-07 07:27:28 +0300

    change file structure

---
+++ src/data/flowers.data.js
@@ -0,0 +1,25 @@
+const flowers =
+    [
+        {
+
+            name: 'rose',
+            _id: 1
+        },
+        {
+
+            name: 'lilac',
+            _id: 2
+        },
+        {
+
+            name: 'lily',
+            _id: 3
+        },
+        {
+
+            name: 'orchid',
+            _id: 4
+        }
+    ]
+
+export default flowers
