35ec75f0d534338eb6fef451ec44835884ba5c01
Alon Mlievski <alonmlievski@gmail.com>
2023-07-07 11:27:20 +0300

    set initial layout

---
+++ src/components/basic/Layout.js
@@ -0,0 +1,18 @@
+import Footer from "./Footer";
+import Header from "./Header";
+
+
+
+
+export default function Layout({ children }) {
+  return (
+    <div>
+      <Header />
+      <main className="min-h-screen text-red-400">
+        {children}
+      </main>
+      <Footer />
+
+    </div>
+  );
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+// pages/index.js
+
+import Link from "next/link"
+
+export default function Home() {
+    return (
+        <div>
+            <h1>test</h1>
+        </div>
+    )
+}
