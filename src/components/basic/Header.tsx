2be00fef42d99d9ae74f43cb5807f24a9830eebd
Alon Mlievski <alonmlievski@gmail.com>
2023-07-09 15:01:38 +0300

    header design

--- src/components/basic/Header.tsx
+++ src/components/basic/Header.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 
 function Header() {
   return (
-<nav>
-  <div>Logo</div>
+<nav className='flex justify-between'>
+  <div className='flex items-center bold'>Yomama</div>
   <div>
-    <ul>
-      <li>page</li>
-      <li>page</li>
-      <li>page</li>
+    <ul className='flex flex-row py-8'>
+      <li className='px-2'>Home</li>
+      <li className='px-2'>About</li>
+      <li className='px-2'>Cart</li>
+      <li className='px-2'>Admin</li>
     </ul>
   </div>
 </nav>
