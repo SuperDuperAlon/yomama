838707766c52cdea4a811ab01829dab3de0a6a14
Alon Mlievski <alonmlievski@gmail.com>
2023-07-11 18:06:50 +0300

    admin crud layout

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -18,13 +18,38 @@ function Admin() {
 
   if (!flowers) return <div>There are no flowers</div>
   else return (
-    <div className="">
-      {flowers.map((flower) => (
+    <div className="bg-red-200">
+      {/* {flowers.map((flower) => (
         <div key={flower._id} className="capitalize">{flower.name} ${flower.price}
           <button onClick={() => {console.log('remove')}}>X</button>
           <button onClick={() => {console.log('edit')}}>Edit</button>
         </div>
-      ))}
+      ))} */}
+      <table>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>Image</th>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Price</th>
+            <th colSpan="2">Actionsssss</th>
+          </tr>
+        </thead>
+        <tbody>
+          {flowers.map((flower, idx) => (
+            <tr key={flower._id}>
+              <td>{idx + 1}</td>
+              <td>Img</td>
+              <td>{flower._id}</td>
+              <td>{flower.name}</td>
+              <td>{flower.price}</td>
+              <td><button onClick={() => {console.log('remove')}}>Remove</button> </td>
+              <td><button onClick={() => {console.log('edit')}}>Edit</button></td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   )
 }
