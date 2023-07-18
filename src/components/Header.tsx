ba4a83bfe3cf4bf294c4176048bb9beebe545f13
Alon Mlievski <alonmlievski@gmail.com>
2023-07-18 13:50:35 +0300

    update header links

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,4 @@
+import Link from 'next/link'
 import React from 'react'
 
 function Header() {
@@ -8,10 +9,10 @@ function Header() {
         <div className='flex items-center bold'>Yomama</div>
         <div>
           <ul className='flex flex-row py-8'>
-            <li className='px-2'>Home</li>
-            <li className='px-2'>About</li>
-            <li className='px-2'>Cart</li>
-            <li className='px-2'>Admin</li>
+            <li className='px-2'><Link href='/'>Home</Link></li>
+            <li className='px-2'><Link href='/'>About</Link></li>
+            <li className='px-2'><Link href='/'>Cart</Link></li>
+            <li className='px-2'><Link href='/admin'>Admin</Link></li>
           </ul>
         </div>
       </div>
