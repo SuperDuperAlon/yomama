b50ce07d175d4069014cc5b99ab127bc8fceafe1
Alon Mlievski <alonmlievski@gmail.com>
2023-07-26 14:57:23 +0300

    form design

--- src/components/admin-flower-create.jsx
+++ src/components/admin-flower-create.jsx
@@ -1,62 +1,65 @@
 import React, { useState } from 'react'
-import {appService} from '../../appwrite'
+import { appService } from '../../appwrite'
 
-const AdminFlowerCreate = ({loadFlowers}) => {
-        const [flowerData, setFlowerData] = useState({
-          name: '',
-          price: 20,
-        });
-      
-        const handleInputChange = (event) => {
-          const { name, value } = event.target;
-          setFlowerData({ ...flowerData, [name]: value });
-        };
-      
-        const handleSubmit = async (event) => {
-          event.preventDefault();
-      
-          try {
-            const response = await appService.create(flowerData);
-            console.log('New Flower Object:', response);
-            loadFlowers()
-      
-            setFlowerData({ name: '', price: 20 });
-          } catch (error) {
-            console.error(error.message);
-          }
-        };
+const AdminFlowerCreate = ({ loadFlowers }) => {
+  const [flowerData, setFlowerData] = useState({
+    name: '',
+    price: 20,
+  });
 
-        console.log(flowerData);
-      
-        return (
-          <div className='flex flex-row'>
-            <h2>Create a New Flower</h2>
-            <form onSubmit={handleSubmit}>
-              <label>
-                Name:
-                <input
-                  type="text"
-                  name="name"
-                  value={flowerData.name}
-                  onChange={handleInputChange}
-                  required
-                />
-              </label>
-              <label>
-                Price:
-                <input
-                  type="number"
-                  name="price"
-                  value={flowerData.price}
-                  onChange={handleInputChange}
-                  required
-                />
-              </label>
-              <button type="submit">Create Flower</button>
-            </form>
-          </div>
-        );
-      };
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFlowerData({ ...flowerData, [name]: value });
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await appService.create(flowerData);
+      console.log('New Flower Object:', response);
+      loadFlowers()
+
+      setFlowerData({ name: '', price: 20 });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+
+  console.log(flowerData);
+
+  return (
+    <div className='flex flex-col'>
+      <h2>Create a New Flower</h2>
+      <div>
+        <form onSubmit={handleSubmit} >
+
+          <label >
+            Name:
+            <input
+              type="text"
+              name="name"
+              value={flowerData.name}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          <label>
+            Price:
+            <input
+              type="number"
+              name="price"
+              value={flowerData.price}
+              onChange={handleInputChange}
+              required
+            />
+          </label>
+          <button type="submit">Create Flower</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 
 export default AdminFlowerCreate
