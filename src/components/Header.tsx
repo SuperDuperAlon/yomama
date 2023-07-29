3ae638441f125d1ba3bb2b04cd79caeb79c56596
Alon Mlievski <alonmlievski@gmail.com>
2023-07-29 18:32:12 +0300

    initial header design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,20 +3,34 @@ import React from 'react'
 
 function Header() {
   return (
-
-    <nav className='bg-blue-100 index-layout full' >
-      <div className='flex flex-row justify-between' >
-        <div className='flex items-center bold logo text-3xl'>Yomama</div>
+    <header className='index-layout full'>
+      <div className='flex justify-between flex-row'>
+        <div className="logo">Yomama</div>
         <div>
-          <ul className='flex flex-row py-8'>
-            <li className='px-2'><Link href='/'>Home</Link></li>
-            <li className='px-2'><Link href='#'>About</Link></li>
-            <li className='px-2'><Link href='#'>Cart</Link></li>
-            <li className='px-2'><Link href='/admin'>Admin</Link></li>
-          </ul>
+          <input type="text" className=''/>
+        </div>
+        <div className='flex flex-row'>
+          <div>user button</div>
+          <div>Fav</div>
+          <div>Cart</div>
         </div>
       </div>
-    </nav>
+
+    </header>
+
+    // <nav className='index-layout full' >
+    //   <div className='flex flex-row justify-between' >
+    //     <div className='flex items-center bold logo text-3xl'>Yomama</div>
+    //     <div>
+    //       <ul className='flex flex-row py-8'>
+    //         <li className='px-2'><Link href='/'>Home</Link></li>
+    //         <li className='px-2'><Link href='#'>About</Link></li>
+    //         <li className='px-2'><Link href='#'>Cart</Link></li>
+    //         <li className='px-2'><Link href='/admin'>Admin</Link></li>
+    //       </ul>
+    //     </div>
+    //   </div>
+    // </nav>
   )
 }
 
