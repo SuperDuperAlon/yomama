005a97d38ecc6cc8d2896e37906bf7b1045ee1b2
Alon Mlievski <alonmlievski@gmail.com>
2023-07-31 23:01:14 +0300

    hero design

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,21 +3,16 @@
 import Image from "next/image";
 import { useEffect } from "react";
 import React from "react";
+import Hero from '@/components/main-hero'
 
 export default function Home() {
 
 
 
     return (
-        <div className="full">
-            <div className="h-screen relative hero">
-                <Image
-                    src="/images/hero-image.jpg"
-                    alt="Hero"
-                    fill={true}
-                />
-            </div>
-        </div>
+        <>
+            <Hero />
+        </>
     )
 }
 
---
+++ src/components/main-hero.jsx
@@ -0,0 +1,17 @@
+import Image from 'next/image'
+import React from 'react'
+
+const Hero = () => {
+    return (
+        <div className="full">
+            <div className="hero">
+                <div className='center'>
+                    <div className='text-white'>Buy Fresh Flowers Today</div>
+                    <button>Check our catalogue</button>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Hero
