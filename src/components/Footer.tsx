a3d3d48da3bcf753a189cfa54f974352e0388410
Alon Mlievski <alonmlievski@gmail.com>
2023-08-01 09:33:38 +0300

    footer design

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,39 @@
 import React from 'react'
 
+const contact = [{
+  icon: 'i',
+  content: 'Shderot Hen 20, Tel Aviv, Israel'
+},
+{
+  icon: 'i',
+  content: 'open for work'
+},
+{
+  icon: 'i',
+  content: '+972-50-443-8778'
+},
+{
+  icon: '',
+  content: 'alonmlievski@gmail.com'
+}]
+
 function Footer() {
   return (
     <footer className=''>
-      <div className='flex justify-center'>
-      <h3 className=''>Made by Alon Mlievski</h3>
+      <div className='flex flex-row justify-between'>
+        <div>YOMAMA</div>
+        <div>Contact
+          <ul>
+            {contact.map((con, idx)=> (
+              <li key={idx} className='flex flex-row'>
+                <div>{con.icon}</div>
+                <div>{con.content}</div>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div>Information</div>
+        <div>Whatsapp</div>
       </div>
     </footer>
   )
