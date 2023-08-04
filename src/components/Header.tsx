323a4f014c2229993a959947ff349373569d34fc
Alon Mlievski <alonmlievski@gmail.com>
2023-08-04 20:15:12 +0300

    header design

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,14 +4,20 @@ import { icons } from '@/styles/assets/icons/icons'
 
 function Header() {
 
+  const links = [
+    { name: 'Products' },
+    { name: 'About' },
+    { name: 'Workshop' },
+    { name: 'Contact' }
+  ]
 
   return (
     <header className='index-layout full'>
       <div className='flex justify-between flex-row top-header'>
         <div className="logo">Yomama</div>
         <div className='flex flex-row main-input'>
-        {icons.magnifyingGlassIcon}
-        <input type="text" className='' placeholder='Search'/>
+          {icons.magnifyingGlassIcon}
+          <input type="text" className='' placeholder='Search' />
         </div>
         <div className='flex flex-row header-nav'>
           <div>{icons.userIcon}</div>
@@ -21,30 +27,13 @@ function Header() {
       </div>
       <div className='break-line full'></div>
       <div className='bottom-header flex'>
-        <ul className='flex flex-row'>
-          <li>Products</li>
-          <li>About</li>
-          <li>Workshop</li>
-
-          <li>Contact</li>
+        <ul className='flex flex-row gap-6'>
+          {links.map((link, idx) => (
+            <li key={idx}>{link.name}</li>
+          ))}
         </ul>
       </div>
-
     </header>
-
-    // <nav className='index-layout full' >
-    //   <div className='flex flex-row justify-between' >
-    //     <div className='flex items-center bold logo text-3xl'>Yomama</div>
-    //     <div>
-    //       <ul className='flex flex-row py-8'>
-    //         <li className='px-2'><Link href='/'>Home</Link></li>
-    //         <li className='px-2'><Link href='#'>About</Link></li>
-    //         <li className='px-2'><Link href='#'>Cart</Link></li>
-    //         <li className='px-2'><Link href='/admin'>Admin</Link></li>
-    //       </ul>
-    //     </div>
-    //   </div>
-    // </nav>
   )
 }
 
