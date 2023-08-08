e158f230a14ab46d318c71bd7d64c720952e4bf6
Alon Mlievski <alonmlievski@gmail.com>
2023-08-08 17:23:11 +0300

    main page design

--- src/components/main-product-list.jsx
+++ src/components/main-product-list.jsx
@@ -29,32 +29,35 @@ const MainProductList = () => {
 
 
 
-<div className="index-layout">
+            <div className="index-layout">
 
-            <ul className="flex">
-                {flowers.map((flower, idx) => (
-                    <li key={idx} className="w-fit flex mr-6">
-                        <div className="w-26 h-56">
-                            <Image
-                                src='/images/top-view-beautiful-roses-bouquet-with-pink-ribbon_23-2148822309.avif'
-                                width={1280}
-                                height={240}
-                                loading="lazy" />
-                            <div className="flex flex-row">
-                            </div>
-                            <div>
-                                <div>{flower.name}</div>
-                                <div>${flower.price}</div>
-                            </div>
-                            <div>
-                                <button>Heart</button>
-                                <button>Cart</button>
-                            </div>
-                        </div>
-                    </li>
-                ))}
-            </ul>
-                </div>
+                <ul className="flex">
+                    {flowers.map((flower, idx) => {
+                        if (idx < 4) {
+                            return (
+
+
+                                <li key={idx} className="flex justify-between">
+                                    <div className="">
+                                        <Image
+                                            src='/images/top-view-beautiful-roses-bouquet-with-pink-ribbon_23-2148822309.avif'
+                                            width={1280}
+                                            height={240}
+                                            loading="lazy" />
+                                        <div className="flex flex-row justify-between">
+                                            <div className="capitalize">{flower.name}</div>
+                                            <div>${flower.price}</div>
+                                        </div>
+                                        <div>
+                                            <button>Heart</button>
+                                            <button>Cart</button>
+                                        </div>
+                                    </div>
+                                </li>)
+                        }
+                    })}
+                </ul>
+            </div>
         </>
     )
 }
