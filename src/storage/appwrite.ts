1c2478b37d5a739820c878bf7a9ed5e746781875
Alon Mlievski <alonmlievski@gmail.com>
2023-09-27 14:45:14 +0300

    change appwrite file to ts

--- src/storage/appwrite.ts
+++ src/storage/appwrite.ts
@@ -6,13 +6,13 @@ const databases = new Databases(client);
 
 client
   .setEndpoint('https://cloud.appwrite.io/v1') // Your API Endpoint
-  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID) // Your project ID
+  .setProject(process.env.NEXT_PUBLIC_PROJECT_ID!) // Your project ID
   ;
 
 async function query() {
   try {
     // console.log(filterBy);
-    const query = await databases.listDocuments(process.env.NEXT_PUBLIC_DATABASE_ID, process.env.NEXT_PUBLIC_COLLECTION_ID,
+    const query = await databases.listDocuments(process.env.NEXT_PUBLIC_DATABASE_ID!, process.env.NEXT_PUBLIC_COLLECTION_ID!,
       // [
       //   Query.equal('name', filterBy),
       // ]
@@ -24,9 +24,9 @@ async function query() {
   }
 }
 
-async function getById(id) {
+async function getById(id: string) {
   try {
-    const query = await databases.getDocument(process.env.NEXT_PUBLIC_DATABASE_ID, process.env.NEXT_PUBLIC_COLLECTION_ID, id);
+    const query = await databases.getDocument(process.env.NEXT_PUBLIC_DATABASE_ID!, process.env.NEXT_PUBLIC_COLLECTION_ID!, id);
     const data = query.documents
     return data
   } catch (error) {
@@ -34,7 +34,7 @@ async function getById(id) {
   }
 }
 
-async function create(newFlower) {
+async function create(newFlower: any) {
   try {
     // Create a new flower object
     const flower = {
@@ -44,31 +44,31 @@ async function create(newFlower) {
 
     // Use Appwrite to create a new document in the "flowers" collection (replace with your collection name)
     const response = await databases.createDocument(
-      process.env.NEXT_PUBLIC_DATABASE_ID,
-      process.env.NEXT_PUBLIC_COLLECTION_ID,
+      process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_COLLECTION_ID!,
       ID.unique(),
       flower
     );
 
     return response;
-  } catch (error) {
+  } catch (error: any) {
     throw new Error('Error creating flower: ' + error.message);
   }
 }
 
-async function remove(id) {
+async function remove(id: string) {
   try {
-    await databases.deleteDocument(process.env.NEXT_PUBLIC_DATABASE_ID,
-      process.env.NEXT_PUBLIC_COLLECTION_ID, id);
+    await databases.deleteDocument(process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_COLLECTION_ID!, id);
   } catch (error) {
     console.log(error);
   }
 }
 
-async function update(id) {
+async function update(id: string) {
   try {
-    await databases.updateDocument(process.env.NEXT_PUBLIC_DATABASE_ID,
-      process.env.NEXT_PUBLIC_COLLECTION_ID, id);
+    await databases.updateDocument(process.env.NEXT_PUBLIC_DATABASE_ID!,
+      process.env.NEXT_PUBLIC_COLLECTION_ID!, id);
   } catch (error) {
     console.log(error);
   }
