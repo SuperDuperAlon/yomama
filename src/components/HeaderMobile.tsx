76a4a37cbda8eb108c2da9a0df5cadb46f1cdf82
Alon Mlievski <alonmlievski@gmail.com>
2023-10-03 13:31:59 +0300

    mobile navbar functionality

--- src/components/HeaderMobile.tsx
+++ src/components/HeaderMobile.tsx
@@ -1,15 +1,18 @@
 'use client'
+import Link from 'next/link'
 import React from 'react'
 import { useState } from 'react'
 import { AiOutlineMenu } from 'react-icons/ai'
+import { arrays } from '@/lib/arrays'
 
 const HeaderMobile = () => {
 
-    const [isOpen, setIsOpen] = useState<boolean>(false)
+    const { headerLinks } = arrays
+
+    const [isOpen, setIsOpen] = useState<boolean>(true)
     const toggleMobileNav = () => {
         setIsOpen(!isOpen)
         console.log(isOpen);
-        
     }
 
     return (
@@ -19,15 +22,23 @@ const HeaderMobile = () => {
                     Yomama
                 </div>
                 <div className='my-4'>
-                        <button onClick={toggleMobileNav}>
-                            <AiOutlineMenu />
-                        </button>
+                    <button onClick={toggleMobileNav}>
+                        <AiOutlineMenu />
+                    </button>
                 </div>
-                <div className={`absolute ease-in-out duration-300 ${isOpen ? '-translate-y-0' : '-translate-y-full'}`}>
-                    <li>option</li>
-                    <li>option</li>
-                    <li>option</li>
-                    <li>option</li>
+                <div className={`absolute left-0 bg-white h-screen w-screen ease-in-out duration-300 ${isOpen ? '-translate-y-0' : '-translate-y-full'}`}>
+                    <button className='absolute right-10 top-4' onClick={toggleMobileNav}>X</button>
+                    <div className='flex h-full'>
+                        <ul className='flex flex-col clean-list justify-center items-center w-full'>
+                            {headerLinks.map((link, idx) => (
+                                <li key={idx}>
+                                    <Link href='/'>
+                                        {link.name}
+                                    </Link>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
                 </div>
             </div>
         </header>
