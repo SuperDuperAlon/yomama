08977302cac2330bea97fcda180b93d9b4f1e7e5
Alon Mlievski <alonmlievski@gmail.com>
2023-12-27 10:50:14 +0200

    responsivity function update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,49 @@ import HeaderDesktop from './HeaderDesktop'
 import HeaderMobile from './HeaderMobile'
 import ScreenWidthListener from "@/lib/screenSizeCalculator"
 
+import { useState, useCallback, useEffect } from 'react';
+
+const useMediaQuery = (width: any) => {
+  const [targetReached, setTargetReached] = useState(false);
+
+  const updateTarget = useCallback((e: any) => {
+    if (e.matches) {
+      setTargetReached(true);
+    } else {
+      setTargetReached(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    const media = window.matchMedia(`(max-width: ${width}px)`);
+    media.addListener(updateTarget);
+
+    // Check on mount (callback is not called until a change occurs)
+    if (media.matches) {
+      setTargetReached(true);
+    }
+
+    return () => media.removeListener(updateTarget);
+  }, []);
+
+  return targetReached;
+};
+
 const Header = () => {
-  const screenSize = ScreenWidthListener()
-  const header = screenSize > 640 ? <HeaderDesktop /> : <HeaderMobile />
+  const isBreakpoint = useMediaQuery(768)
   return (
     <>
-    {/* <HeaderDesktop /> */}
-      {header}
+      <div>
+        {isBreakpoint ? (
+          <div>
+            <HeaderMobile />
+          </div>
+        ) : (
+          <div>
+            <HeaderDesktop />
+          </div>
+        )}
+      </div>
     </>
   )
 }
