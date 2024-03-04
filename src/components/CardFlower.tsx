d0bfb623bc089b3f61c7dc0b5a415bdc18e82065
Alon Mlievski <alonmlievski@gmail.com>
2024-03-04 17:39:52 +0200

    add an image to a Card

--- src/components/CardFlower.tsx
+++ src/components/CardFlower.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import React from 'react'
 
 interface PropsCard {
@@ -8,13 +9,21 @@ const CardFlower = (flower: PropsCard) => {
     return (
         <>
             <div>
-                <img src="" alt="" />
+                <Image
+                    src='/images/top-view-beautiful-roses-bouquet-with-pink-ribbon_23-2148822309.avif'
+                    className=""
+                    width={180}
+                    height={120}
+                    alt="flower name"
+                    loading="lazy"
+                />
                 <div>
                     {flower.flower.name}
                 </div>
                 <div>
                     {flower.flower.price}
                 </div>
+
             </div>
         </>
 
